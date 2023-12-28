d73f20984e4d77494f88b96c1518638f3a43fed0
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2023-12-28 16:40:24 +0530

    users added

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true }));
 
-
-
 app.post("/authenticate", async (req, res) => {
   const { username } = req.body;
-
-
   try{
     const r = await axios.put(
       'https://api.chatengine.io/users/',
-      {username: username,secret: username, first_name: username},
+      {username: username,secret: username,first_name: username},
       {headers: {"private-key": "a9e31964-a7cd-4f6d-8b49-6fe13c29041b"}}
     )
     return res.status(r.status).json(r.data)
   } catch(e){
-
+    return res.status(e.response.status).json(e.response.data)
   }
-
-
-  return res.json({ username: username, secret: "sha256..." });
+  // return res.json({ username: username, secret: "sha256..." });
 });
 
 app.listen(3001);
